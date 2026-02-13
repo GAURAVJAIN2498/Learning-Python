80fe0120ea45af3cbbea92ea6e26c50cab02aade
gaurav jain <gauravjain2498@gmail.com>
2026-02-13 18:05:34 +0530

    new files(13-02-2026)

--- 5.py
+++ 5.py
@@ -4,6 +4,11 @@ print(s)
 
 
 name = ['vishal', 'mehul@ongraph.com', 'gaurav', 'sachin', '']
-final_email = [n + "@company.com" for n in name if not n.endswith('@ongraph.com')]
+final_email = [n + "@company.com" for n in name if not n.endswith("ongraph.com")]
+print(final_email)
+
+final_email = [n if n.endswith('@ongraph.com') 
+    else (n + "@company.com")
+    for n in name]
 print(final_email)
 
---
+++ 6.py
@@ -0,0 +1,30 @@
+# x = 5
+# y = 10
+# print(y/x)  #shorthand operation
+# print(y//x) #floor division
+
+
+
+# page = 1
+# while page >= 1 and page <= 15:
+#     if page == 5:
+#      print("skipping page 5")
+     
+#     else:        
+#         print("processing page")  
+#     page += 1
+                       
+# else:
+#     print("no page")      
+
+
+page = 15
+if page > 10:
+    print("No page")
+else:
+    while page <= 10:
+        if page == 5:
+            print("Skipping page")
+        else:
+            print("Processing page")
+        page += 1
