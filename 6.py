5b6804b074406e6993d3aec48cdbc3995d8f921f
gaurav jain <gauravjain2498@gmail.com>
2026-02-17 18:14:37 +0530

    17-2-2026

--- 6.py
+++ 6.py
@@ -3,13 +3,11 @@
 # print(y/x)  #shorthand operation
 # print(y//x) #floor division
 
-
-
 # page = 1
 # while page >= 1 and page <= 15:
 #     if page == 5:
 #      print("skipping page 5")
-     
+    
 #     else:        
 #         print("processing page")  
 #     page += 1
