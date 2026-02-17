86081ee92ef30858e42269985bd2bcc171e538f7
gaurav jain <gauravjain2498@gmail.com>
2026-02-17 17:19:32 +0530

    17-07-2026

--- 11.py
+++ 11.py
@@ -1,7 +1,13 @@
 def lw(i, j):
     print("Learning Python")
-    return [i + j, i-j, i*j, i/j]
+    # return [i + j, i-j, i*j, i/j]
+    return(i + j, i-j, i*j, i/j)  
     print("This will not be printed")   # This line will not be executed as it is after the return statement    
 print(lw(18, 5))
 
 
+def data(city, country, continent):
+    print("ur city is : " + city)
+    print("ur country is : " + country)
+    print("ur  in : " + continent)
+data("Mumbai", "India", "Asia")
