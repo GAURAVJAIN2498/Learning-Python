6de2b34de52f3e0c02730513049f4c025dba475f
gaurav jain <gauravjain2498@gmail.com>
2026-02-17 18:02:25 +0530

    17-07-2026

--- 11.py
+++ 11.py
@@ -10,4 +10,21 @@ def data(city, country, continent):
     print("ur city is : " + city)
     print("ur country is : " + country)
     print("ur  in : " + continent)
-data("Mumbai", "India", "Asia")
+# data("Mumbai", "India", "Asia")
+
+data("New York", "USA", "North America") #named arguments
+
+
+print("Learning Python", end=" going well")
+print(" This is a", sep= "\n" "next line")  
+
+
+
+def jobtitle():
+    return "Software Engineer"
+def notjoining():
+    return "Not Joining"
+
+salary = 100000
+newjoin = jobtitle() if salary > 50000 else notjoining()
+print(newjoin)
